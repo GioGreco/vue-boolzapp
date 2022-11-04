43127542a3a4b2415560544e32d24bda28886d6a
GioGreco <grecogiorgio.97@gmail.com>
2022-11-04 17:51:32 +0100

    add messages transition

--- js/script.js
+++ js/script.js
@@ -70,18 +70,21 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id: 1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {   
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
+                            id:3,
                             showOptions: false,
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
@@ -97,18 +100,21 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
+                            id:3,
                             showOptions: false,
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
@@ -124,18 +130,21 @@ const app = createApp({
                     visible: true,
                     messages: [
                         {
+                            id:1,
                             showOptions: false,
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         }, 
                         {
+                            id:3,
                             showOptions: false,
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
@@ -151,12 +160,14 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
@@ -172,12 +183,14 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
@@ -193,18 +206,21 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
+                            id:3,
                             showOptions: false,
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
@@ -220,12 +236,14 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
@@ -241,18 +259,21 @@ const app = createApp({
                     visible: true,
                     messages:[
                         {
+                            id:1,
                             showOptions: false,
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
+                            id:2,
                             showOptions: false,
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
+                            id:3,
                             showOptions: false,
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
@@ -314,7 +335,6 @@ const app = createApp({
         },
         removeMessage(contact, message){
             contact.messages.splice(message, 1);
-            console.log(contact);
         },
         parsedDate(d){
             let parsed = dt.fromFormat(d, 'dd/LL/yyyy hh:mm:ss').toFormat('MMMM dd yyyy, HH:mm');
